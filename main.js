ec3ed2aeff27d892d6393a5cf47b3ec04992ce48
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-15 22:22:40 -0700

    add option to create custom grid

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
-function makeGrid() {
+function makeGrid(squaresPerRow = 4) {
   const gridContainer = document.getElementById("grid-container");
-  for (i = 0; i < 16; i++) {
+  // remove existing elements, if any
+  removeChildElements(gridContainer);
+
+  for (i = 0; i < squaresPerRow ** 2; i++) {
     let square = document.createElement("div");
     square.className = "square";
     gridContainer.append(square);
+  }
+
+  addMouseoverToSquares();
+}
+
+function removeChildElements(parent) {
+  while (parent.hasChildNodes()) {
+    parent.removeChild(parent.lastChild);
   }
 }
 
 function onHover(square) {
   square.style.backgroundColor = "blue";
-  console.log("hoover");
 }
 
-// makeGrid() has to come before event listeners are made
-// otherwise query selector won't have any div.square's to grab
-makeGrid();
+function makeCustomGrid() {
+  let squaresPerRow = prompt("How many squares would you like on each row? ex. 5 = 5x5 grid");
+  makeGrid(squaresPerRow);
+}
 
-const squares = document.querySelectorAll("div.square");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", onHover.bind(event, square));
-  console.log("yep");
-});
+function addMouseoverToSquares() {
+  const squares = document.querySelectorAll("div.square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", onHover.bind(event, square));
+  });
+}
+
+const makeGridButton = document.querySelector("button.make-grid-button");
+makeGridButton.addEventListener("click", makeCustomGrid);
+
+makeGrid();
